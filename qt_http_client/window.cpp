86554fc033794a09c476908bf1c9c7b4fe354a83
Jon Mohrbacher <johnnymo87@gmail.com>
2020-06-03 14:27:48 -0400

    Debug all signals

--- qt_http_client/window.cpp
+++ qt_http_client/window.cpp
@@ -32,6 +32,43 @@ Window::Window(QWidget *parent) : QWidget(parent)
     connect(m_button, SIGNAL (clicked(bool)), this, SLOT (slotButtonClicked(bool)));
     connect(m_comboBox, &QComboBox::currentTextChanged, this, &Window::slotCurrentTextChanged);
     connect(m_networkAccessManager, &QNetworkAccessManager::finished, this, &Window::slotHttpRequestFinished);
+
+
+    // Debug all signals to network access manager
+    connect(m_networkAccessManager, &QNetworkAccessManager::authenticationRequired, this,
+    [=](QNetworkReply *reply, QAuthenticator *authenticator){
+        qDebug() << "networkAccessManager authenticationRequired";
+    });
+    connect(m_networkAccessManager, &QNetworkAccessManager::encrypted, this,
+    [=](QNetworkReply *reply){
+        qDebug() << "networkAccessManager encrypted";
+    });
+    connect(m_networkAccessManager, &QNetworkAccessManager::finished, this,
+    [=](QNetworkReply *reply){
+        qDebug() << "networkAccessManager finished";
+    });
+    connect(m_networkAccessManager, &QNetworkAccessManager::networkAccessibleChanged, this,
+    [=](QNetworkAccessManager::NetworkAccessibility accessible){
+        qDebug() << "networkAccessManager networkAccessibleChanged";
+    });
+    connect(m_networkAccessManager, &QNetworkAccessManager::preSharedKeyAuthenticationRequired, this,
+    [=](QNetworkReply *reply, QSslPreSharedKeyAuthenticator *authenticator){
+        qDebug() << "networkAccessManager preSharedKeyAuthenticationRequired";
+    });
+    connect(m_networkAccessManager, &QNetworkAccessManager::proxyAuthenticationRequired, this,
+    [=](const QNetworkProxy &proxy, QAuthenticator *authenticator){
+        qDebug() << "networkAccessManager proxyAuthenticationRequired";
+    });
+    connect(m_networkAccessManager, &QNetworkAccessManager::sslErrors, this,
+    [=](QNetworkReply *reply, const QList<QSslError> &errors){
+        qDebug() << "networkAccessManager sslErrors";
+    });
+    connect(m_networkAccessManager, &QObject::destroyed, [] {
+        qDebug() << "networkAccessManager got deleted!";
+    });
+    connect(this, &QObject::destroyed, [] {
+        qDebug() << "Window got deleted!";
+    });
 }
 
 void Window::slotButtonClicked(bool checked)
@@ -50,6 +87,52 @@ void Window::slotButtonClicked(bool checked)
         m_redirectPolicy
     );
     m_networkReply = m_networkAccessManager->post(m_networkRequest, data.toUtf8());
+
+
+    // Debug all signals to network reply
+    connect(m_networkReply, &QNetworkReply::downloadProgress, this,
+    [=](qint64 bytesReceived, qint64 bytesTotal){
+        qDebug() << "networkReply downloadProgress";
+    });
+    connect(m_networkReply, &QNetworkReply::encrypted, this,
+    [=](){
+        qDebug() << "networkReply encrypted";
+    });
+    connect(m_networkReply, QOverload<QNetworkReply::NetworkError>::of(&QNetworkReply::error), this,
+    [=](QNetworkReply::NetworkError code){
+        qDebug() << "networkReply errorOccurred";
+    });
+    connect(m_networkReply, &QNetworkReply::finished, this,
+    [=](){
+        qDebug() << "networkReply finished";
+    });
+    connect(m_networkReply, &QNetworkReply::metaDataChanged, this,
+    [=](){
+        qDebug() << "networkReply metaDataChanged";
+    });
+    connect(m_networkReply, &QNetworkReply::preSharedKeyAuthenticationRequired, this,
+    [=](QSslPreSharedKeyAuthenticator *authenticator){
+        qDebug() << "networkReply preSharedKeyAuthenticationRequired";
+    });
+    connect(m_networkReply, &QNetworkReply::redirectAllowed, this,
+    [=](){
+        qDebug() << "networkReply redirectAllowed";
+    });
+    connect(m_networkReply, &QNetworkReply::redirected, this,
+    [=](const QUrl &url){
+        qDebug() << "networkReply redirected";
+    });
+    connect(m_networkReply, &QNetworkReply::sslErrors, this,
+    [=](const QList<QSslError> &errors){
+        qDebug() << "networkReply sslErrors";
+    });
+    connect(m_networkReply, &QNetworkReply::uploadProgress, this,
+    [=](qint64 bytesSent, qint64 bytesTotal){
+        qDebug() << "networkReply uploadProgress";
+    });
+    connect(m_networkReply, &QObject::destroyed, [] {
+        qDebug() << "networkReply got deleted!";
+    });
 }
 
 
